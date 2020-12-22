7bf7c28bf67a5ca698836dfdf5df5bc2ae32b509
Ashes Mondal <ashes.m2001@gmail.com>
2020-12-22 09:24:28 +0530

    configured db

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoDbURL:"mongodb+srv://winter-project:MUrcx09g23LwMHf3@winterprojectcluster0.uuugz.mongodb.net/ecommerceDb?retryWrites=true&w=majority"
+    mongoDbURL:"mongodb+srv://winter-project:<PASSWORD>@winterprojectcluster0.uuugz.mongodb.net/ecommerceDb?retryWrites=true&w=majority",
+    // mongoDbPWD:"MUrcx09g23LwMHf3"
 };
