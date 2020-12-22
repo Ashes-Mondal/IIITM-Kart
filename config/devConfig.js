b1b8a4774e9e7a5e8a98494f76ff24cdf214f7b3
Ashes Mondal <ashes.m2001@gmail.com>
2020-12-22 14:32:51 +0530

    mongoDb key config

---
+++ config/devConfig.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoDbURL:"mongodb+srv://winter-project:MUrcx09g23LwMHf3@winterprojectcluster0.uuugz.mongodb.net/ecommerceDb?retryWrites=true&w=majority",
+    // mongoDbPWD:"MUrcx09g23LwMHf3"
+};
---
+++ config/productionConfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoDbURL:process.env.MONGO_URL,
+};
