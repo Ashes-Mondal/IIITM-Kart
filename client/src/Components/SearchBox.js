229df107f003442e822d25310d73c9084883dc84
Subodh Rajpopat <subodhr2001@gmail.com>
2020-12-22 13:47:23 +0530

    fix:Product Cards appearance

--- client/src/Components/SearchBox.js
+++ client/src/Components/SearchBox.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const SearchBox = () => {
-    return (
-        <div>
-            <input
-                className = "form-control p-2 m-4"
-                type='search'
-                placeholder='search robots'
-            />
-        </div>
-
-    );
-}
+  return (
+    <div>
+      <input
+        className="form-control p-2 m-4"
+        type="search"
+        placeholder="search products"
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
