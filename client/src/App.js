1e75e7810e404f9b04878d0a96e839d5b41b0ded
Ashes Mondal <ashes.m2001@gmail.com>
2020-12-23 12:09:21 +0530

    background color changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,43 @@
-import React,{useReducer,useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CardList from './Components/CardList';
-import SearchBox from './Components/SearchBox';
+import React, { useReducer, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CardList from "./Components/CardList";
+import SearchBox from "./Components/SearchBox";
 import "./App.css";
 
-const reducer = (currState,action)=>{
-    switch(action.type){
-        case 'fetchedItemJson':
-            console.log("dispatch-ItemData",action.payload);
-            return action.payload
-        default:
-            return currState
-    };
-}
+const reducer = (currState, action) => {
+  switch (action.type) {
+    case "fetchedItemJson":
+      console.log("dispatch-ItemData", action.payload);
+      return action.payload;
+    default:
+      return currState;
+  }
+};
 
 const App = () => {
-    const [itemList, dispatch] = useReducer(reducer, []);
+  const [itemList, dispatch] = useReducer(reducer, []);
 
-    //side effect when page first time rendered
-    useEffect(() => {
-        const URL = "/fetchItems";
+  //side effect when page first time rendered
+  useEffect(() => {
+    const URL = "/fetchItems";
 
-        const fetchJSON_fromServer = async ()=>{
-            const itemList = await (await fetch(URL)).json()
-            dispatch({type:"fetchedItemJson",payload:itemList});
-        }
-        fetchJSON_fromServer();
-    }, [])
+    const fetchJSON_fromServer = async () => {
+      const itemList = await (await fetch(URL)).json();
+      dispatch({ type: "fetchedItemJson", payload: itemList });
+    };
+    fetchJSON_fromServer();
+  }, []);
 
-    return (
-        <div class = "container" >
-            <span class="text1">IIITM-Kart</span>
-            <span class="text2">A Shopping site for IIITM students</span>
-            <SearchBox />
-            <CardList itemList={itemList} />
-        </div>
-
-    )
-}
+  return (
+    <div className="container">
+      <div className ="heading">
+        <span className="text1">IIITM-Kart</span>
+        <div className="text2">A Shopping site for IIITM students</div>
+      </div>
+      <SearchBox />
+      <CardList itemList={itemList} />
+    </div>
+  );
+};
 
 export default App;
