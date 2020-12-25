3376076cecee8182cc56aa0af79ef9fd0d1e6991
Ashes Mondal <ashes.m2001@gmail.com>
2020-12-25 22:55:23 +0530

    navbar CSS added

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 const HomePage = ({ itemList }) => {
   return (
-    <div className="col container shadow p-3 mb-5 bg-white rounded">
+    <div className="col container shadow rounded">
       <div className="row justify-content-center ">
         {itemList.map((item, i) => {
           return (
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,31 +1,27 @@
 import React from "react";
 import SearchBox from "./SearchBox";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <div className="heading">
-        <div className="navlinks">
-          <ul>
-            <li>
-              <span className="text1">IIITM-Kart</span>
-              <span className="text2">A Shopping site for IIITM students</span>
-            </li>
-            <li>
-              <SearchBox />
-            </li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/cart">Cart</Link>
-            </li>
-            <li>
-              <Link to="/user">User Details</Link>
-            </li>
-          </ul>
-        </div>
+    <nav className="nav-bar navbar sticky-top">
+      <div className="site-name">
+        <span className="text1">IIITM-Kart</span>
+        <span className="text2">A Shopping site for IIITM students</span>
+      </div>
+      <div className="search-box">
+        <SearchBox />
+      </div>
+      <div className="navlink-container">
+        <NavLink className ="navlink" activeClassName="active-nav-link" exact to="/">
+          Home
+        </NavLink>
+        <NavLink className ="navlink" activeClassName="active-nav-link " exact  to="/cart">
+          Cart
+        </NavLink>
+        <NavLink className ="navlink" activeClassName="active-nav-link" exact to="/user">
+          User Details
+        </NavLink>
       </div>
     </nav>
   );
