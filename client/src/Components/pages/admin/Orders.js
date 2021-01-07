3d124fe53caa4edc083e3c0408ee03059cf009ab
Subodh Rajpopat <subodhr2001@gmail.com>
2021-01-07 12:37:30 +0530

    Added CSS styling

--- client/src/Components/pages/admin/Orders.js
+++ client/src/Components/pages/admin/Orders.js
@@ -2,7 +2,7 @@ import React, { useEffect, useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Authentication } from "../../../App";
 
-const Orders = ({ setAdmin ,setCart }) => {
+const Orders = ({ setAdmin, setCart }) => {
   const history = useHistory();
   const { setIsAuth } = useContext(Authentication);
   const [orders, setOrders] = useState([]);
@@ -21,31 +21,34 @@ const Orders = ({ setAdmin ,setCart }) => {
     fetchAllOrders();
   }, [history, setIsAuth, setAdmin]);
 
- const handleDeliveryStatus = async(order)=>{
-  const customerId = order.user._id;
-  const orderId = order._id;
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      customerId:customerId,
-      orderId:orderId
-    }),
+  const handleDeliveryStatus = async (order) => {
+    const customerId = order.user._id;
+    const orderId = order._id;
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        customerId: customerId,
+        orderId: orderId,
+      }),
+    };
+    const result = await (
+      await fetch("/adminChangeDeliveryStatus", requestOptions)
+    ).json();
+    if (result.response === false) {
+      alert("Could not proceed further!");
+      setIsAuth(false);
+      setCart([]);
+      history.push("/login");
+    } else {
+      let ordersData = orders.map((orderdetails) => {
+        if (orderdetails._id === orderId)
+          order.deliveryStatus = !order.deliveryStatus;
+        return orderdetails;
+      });
+      setOrders(ordersData);
+    }
   };
-  const result = await (await fetch("/adminChangeDeliveryStatus", requestOptions)).json();
-  if (result.response === false) {
-    alert("Could not proceed further!");
-    setIsAuth(false);
-    setCart([]);
-    history.push("/login");
-  }else{
-    let ordersData = orders.map(orderdetails=>{
-      if(orderdetails._id === orderId)order.deliveryStatus = !order.deliveryStatus;
-      return orderdetails;
-    })
-    setOrders(ordersData);
-  }
-  }
 
   return (
     <div className="adminPanel">
@@ -53,7 +56,7 @@ const Orders = ({ setAdmin ,setCart }) => {
       <main>
         {orders.map((order) => {
           return (
-            <div className="container user-form">
+            <div className=" box box-orders user-form">
               {/* ORDER ID */}
               <div>
                 <strong>orderId:</strong>
@@ -65,25 +68,27 @@ const Orders = ({ setAdmin ,setCart }) => {
                   <div style={{ textAlign: "center" }}>
                     <strong>User Details</strong>
                   </div>
-                  <div>
-                    <strong>First Name:</strong>
-                    {order.user.name.firstName}
-                  </div>
-                  <div>
-                    <strong>Last Name:</strong>
-                    {order.user.name.lastName}
-                  </div>
-                  <div>
-                    <strong>Phone:</strong>
-                    {order.user.phone}
-                  </div>
-                  <div>
-                    <strong style={{ textAlign: "center" }}>Email:</strong>
-                    {order.user.email}
-                  </div>
-                  <div>
-                    <strong>Address:</strong>
-                    {order.user.address}
+                  <div className="orderbox">
+                    <div>
+                      <strong>First Name:</strong>
+                      {order.user.name.firstName}
+                    </div>
+                    <div>
+                      <strong>Last Name:</strong>
+                      {order.user.name.lastName}
+                    </div>
+                    <div>
+                      <strong>Phone:</strong>
+                      {order.user.phone}
+                    </div>
+                    <div>
+                      <strong style={{ textAlign: "center" }}>Email:</strong>
+                      {order.user.email}
+                    </div>
+                    <div>
+                      <strong>Address:</strong>
+                      {order.user.address}
+                    </div>
                   </div>
                 </div>
                 {/* PAYMENT DETAILS */}
@@ -91,53 +96,69 @@ const Orders = ({ setAdmin ,setCart }) => {
                   <div style={{ textAlign: "center" }}>
                     <strong>Payment Details</strong>
                   </div>
-                  <div>
-                    <strong>Date Of Order:</strong>
-                    {order.dateOfOrder}
-                  </div>
-                  <div>
-                    <strong>Total Cost:</strong>
-                    {order.totalCost}
-                  </div>
-                  <div>
-                    <strong>razorpay PaymentId:</strong>
-                    {order.razorpayPaymentId}
-                  </div>
-                  <div>
-                    <strong>razorpay OrderId:</strong>
-                    {order.razorpayOrderId}
-                  </div>
-                  <div>
-                    <strong>razorpay Signature:</strong>
-                    {order.razorpaySignature}
+                  <div className="orderbox">
+                    <div>
+                      <strong>Date Of Order:</strong>
+                      {order.dateOfOrder}
+                    </div>
+                    <div>
+                      <strong>Total Cost:</strong>
+                      {order.totalCost}
+                    </div>
+                    <div>
+                      <strong>razorpay PaymentId:</strong>
+                      {order.razorpayPaymentId}
+                    </div>
+                    <div>
+                      <strong>razorpay OrderId:</strong>
+                      {order.razorpayOrderId}
+                    </div>
+                    <div>
+                      <strong>razorpay Signature:</strong>
+                      {order.razorpaySignature}
+                    </div>
                   </div>
                 </div>
                 {/* ORDER */}
                 <div className="order-div">
-                <div style={{ textAlign: "center" }}>
+                  <div style={{ textAlign: "center" }}>
                     <strong>Order Details</strong>
                   </div>
-                  {order.order.map((itemDetails) => {
-                    return (
-                      <div className="order-item-div ">
-                        <div>
-                          <strong>Item Id:</strong>
-                          {itemDetails.item._id}
+                  <div className="orderbox">
+                    {order.order.map((itemDetails) => {
+                      return (
+                        <div className="order-item-div ">
+                          <div>
+                            <strong>Item Id:</strong>
+                            {itemDetails.item._id}
+                          </div>
+                          <div>
+                            <strong>Item Name:</strong>
+                            {itemDetails.item.itemName}
+                          </div>
+                          <div>
+                            <strong>Qty:</strong>
+                            {itemDetails.Qty}
+                          </div>
                         </div>
-                        <div>
-                          <strong>Item Name:</strong>
-                          {itemDetails.item.itemName}
-                        </div>
-                        <div>
-                          <strong>Qty:</strong>
-                          {itemDetails.Qty}
-                        </div>
-                      </div>
-                    );
-                  })}
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
-              <button onClick={()=>{handleDeliveryStatus(order)}}><strong>{order.deliveryStatus?"DELIVERED":"PENDING"}</strong></button>
+              <button
+                onClick={() => {
+                  handleDeliveryStatus(order);
+                }}
+              >
+                <strong>
+                  {order.deliveryStatus ? (
+                    <span className="delivered">DELIVERED</span>
+                  ) : (
+                    <span className="pending">PENDING</span>
+                  )}
+                </strong>
+              </button>
             </div>
           );
         })}
